17a11c6fc160a672c252be19427d05bcc9e532f8
mradisavljevic <mradisavljevicmas@gmail.com>
2023-09-04 18:14:15 +0200

    Updated indicator

--- lib/pages/register_shell.dart
+++ lib/pages/register_shell.dart
@@ -2,10 +2,8 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:kendamanomics_mobile/extensions/custom_colors.dart';
 import 'package:kendamanomics_mobile/extensions/custom_text_styles.dart';
-import 'package:kendamanomics_mobile/widgets/register-shell/register_description.dart';
-import 'package:kendamanomics_mobile/widgets/register-shell/register_input.dart';
-import 'package:kendamanomics_mobile/widgets/register-shell/register_ranking.dart';
-import 'package:kendamanomics_mobile/widgets/register-shell/register_welcome.dart';
+import 'package:kendamanomics_mobile/providers/register_provider.dart';
+import 'package:provider/provider.dart';
 
 class RegisterShell extends StatelessWidget {
   static const pageName = 'register';
@@ -17,23 +15,61 @@ class RegisterShell extends StatelessWidget {
       resizeToAvoidBottomInset: true,
       backgroundColor: CustomColors.of(context).backgroundColor,
       body: SafeArea(
-        child: NotificationListener(
-          // onNotification: (notification) {
-          //   if (notification is ScrollStartNotification) {
-          //     consider adding int index to the pages so we can keep the keyboard up properly even on vertical scroll
-          //     FocusManager.instance.primaryFocus?.unfocus();
-          //   }
-          //   return false;
-          // },
-          child: PageView(
-            children: const [
-              RegisterWelcome(),
-              RegisterDescription(),
-              RegisterRanking(),
-              RegisterInput(),
-            ],
+        child: ChangeNotifierProvider(
+          create: (context) => RegisterProvider(),
+          builder: (context, child) => Consumer<RegisterProvider>(
+            builder: (context, provider, child) => NotificationListener(
+              // onNotification: (notification) {
+              //   if (notification is ScrollStartNotification) {
+              //     consider adding int index to the pages so we can keep the keyboard up properly even on vertical scroll
+              //     FocusManager.instance.primaryFocus?.unfocus();
+              //   }
+              //   return false;
+              // },
+              child: Stack(
+                children: [
+                  Positioned(
+                    bottom: 0,
+                    top: 0,
+                    right: 0,
+                    left: 0,
+                    child: PageView.builder(
+                      controller: PageController(initialPage: provider.currentPage),
+                      itemCount: 4,
+                      onPageChanged: (value) {
+                        provider.setCurrentPage(value);
+                      },
+                      itemBuilder: (context, index) {
+                        return provider.pages[index];
+                      },
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 12.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        provider.pages.length,
+                        (index) => buildIndicator(index, provider.currentPage, context),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildIndicator(int index, int currentPage, BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width / 4,
+      height: 4.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: index == currentPage ? CustomColors.of(context).activeIndicatorColor : Colors.transparent,
       ),
     );
   }
--- lib/providers/register_provider.dart
+++ lib/providers/register_provider.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:kendamanomics_mobile/mixins/logger_mixin.dart';
+import 'package:kendamanomics_mobile/widgets/register-shell/register_description.dart';
+import 'package:kendamanomics_mobile/widgets/register-shell/register_input.dart';
+import 'package:kendamanomics_mobile/widgets/register-shell/register_ranking.dart';
+import 'package:kendamanomics_mobile/widgets/register-shell/register_welcome.dart';
 
 class RegisterProvider extends ChangeNotifier with LoggerMixin {
+  final List<Widget> pages = [
+    const RegisterWelcome(),
+    const RegisterDescription(),
+    const RegisterRanking(),
+    const RegisterInput(),
+  ];
   String _firstName = '';
   String _lastName = '';
   String _email = '';
@@ -10,6 +20,7 @@ class RegisterProvider extends ChangeNotifier with LoggerMixin {
   String _support = '';
   String _password = '';
   String _confirmPassword = '';
+  int _currentPage = 0;
 
   String get firstName => _firstName;
   String get lastName => _lastName;
@@ -19,6 +30,7 @@ class RegisterProvider extends ChangeNotifier with LoggerMixin {
   String get support => _support;
   String get password => _password;
   String get confirmPassword => _confirmPassword;
+  int get currentPage => _currentPage;
 
   set firstName(String value) {
     _firstName = value;
@@ -57,6 +69,11 @@ class RegisterProvider extends ChangeNotifier with LoggerMixin {
 
   set support(String value) {
     _support = value;
+    notifyListeners();
+  }
+
+  void setCurrentPage(int page) {
+    _currentPage = page;
     notifyListeners();
   }
 
