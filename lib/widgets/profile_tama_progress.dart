7557426b051aab716103013dc3a45c23fa5c638e
NikolaCredeo <n.milosevic0111@gmail.com>
2023-11-02 14:20:03 +0100

    fixed tama image loading

--- lib/widgets/profile_tama_progress.dart
+++ lib/widgets/profile_tama_progress.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kendamanomics_mobile/extensions/custom_colors.dart';
 import 'package:kendamanomics_mobile/extensions/custom_text_styles.dart';
 import 'package:kendamanomics_mobile/models/player_tama.dart';
+import 'package:kendamanomics_mobile/models/tama.dart';
 import 'package:kendamanomics_mobile/providers/profile_page_provider.dart';
 import 'package:kendamanomics_mobile/widgets/gif_loading_animation.dart';
 
@@ -29,32 +30,36 @@ class ProfileTamaProgress extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         return LayoutBuilder(
           builder: (context, constraints) {
-            return Row(
+            return Column(
               children: [
-                Column(
+                Row(
                   children: [
-                    Image.asset(
+                    Image.network(
                       playerTamas[index].tama.imageUrl,
                       width: constraints.maxWidth * 0.7,
                       height: constraints.maxWidth * 0.7,
                     ),
-                    Text(
-                      playerTamas[index].tama.tamaGroupName ?? '',
-                      style: CustomTextStyles.of(context).regular14.apply(color: CustomColors.of(context).primary),
-                    ),
-                    Text(
-                      playerTamas[index].tama.name ?? '',
-                      style: CustomTextStyles.of(context).regular14.apply(color: CustomColors.of(context).primary),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 4),
+                      child: Image.asset(
+                        'assets/icon/icon_trophy_default.png',
+                        width: constraints.maxWidth * 0.3,
+                        height: constraints.maxWidth * 0.3,
+                      ),
                     ),
                   ],
                 ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: constraints.maxHeight - 87),
-                  child: Image.asset(
-                    'assets/icon/icon_trophy_completed.png',
-                    width: constraints.maxWidth * 0.3,
-                    height: constraints.maxWidth * 0.3,
-                  ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        getName(playerTamas[index].tama),
+                        textAlign: TextAlign.center,
+                        style: CustomTextStyles.of(context).regular14.apply(color: CustomColors.of(context).primary),
+                      ),
+                    ),
+                    SizedBox(width: constraints.maxWidth * 0.3, height: constraints.maxWidth * 0.3),
+                  ],
                 ),
               ],
             );
@@ -62,5 +67,9 @@ class ProfileTamaProgress extends StatelessWidget {
         );
       },
     );
+  }
+
+  String getName(Tama tama) {
+    return '${tama.tamaGroupName ?? ''} ${tama.name ?? ''}';
   }
 }
