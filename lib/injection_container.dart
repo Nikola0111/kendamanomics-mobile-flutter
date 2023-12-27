0913ecbe1c88e746e6cf94b0b86949f8a7e638da
Nikola Milošević <57261667+Nikola0111@users.noreply.github.com>
2023-12-27 11:07:17 +0100

    implemented fetching and ordering premium tamas (#29)
    * implemented fetching and ordering premium tamas
    
    * Update tamas_provider.dart
    
    * refactor
    
    * Update tamas_provider.dart
    
    * added fetching of purchased tamas groups

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -6,6 +6,7 @@ import 'package:kendamanomics_mobile/services/environment_service.dart';
 import 'package:kendamanomics_mobile/services/leaderboards_service.dart';
 import 'package:kendamanomics_mobile/services/logger_service.dart';
 import 'package:kendamanomics_mobile/services/persistent_data_service.dart';
+import 'package:kendamanomics_mobile/services/purchase_service.dart';
 import 'package:kendamanomics_mobile/services/router_service.dart';
 import 'package:kendamanomics_mobile/services/submission_service.dart';
 import 'package:kendamanomics_mobile/services/supabase_service.dart';
@@ -31,4 +32,5 @@ void initKiwi() {
   KiwiContainer().registerSingleton((container) => UserService());
   KiwiContainer().registerSingleton((container) => CompanyService());
   KiwiContainer().registerSingleton((container) => LoggerService());
+  KiwiContainer().registerSingleton((container) => PurchaseService());
 }
--- lib/services/tamas_group_service.dart
+++ lib/services/tamas_group_service.dart
@@ -1,4 +1,6 @@
+import 'package:kendamanomics_mobile/constants.dart';
 import 'package:kendamanomics_mobile/mixins/logger_mixin.dart';
+import 'package:kendamanomics_mobile/models/premium_tamas_group.dart';
 import 'package:kendamanomics_mobile/models/tamas_group.dart';
 import 'package:kendamanomics_mobile/services/persistent_data_service.dart';
 import 'package:kiwi/kiwi.dart';
@@ -8,19 +10,36 @@ class TamasGroupService with LoggerMixin {
   final _persistantDataService = KiwiContainer().resolve<PersistentDataService>();
   final _supabase = Supabase.instance.client;
 
-  Future<List<TamasGroup>?> fetchTamaGroups() async {
+  Future<Map<String, List<TamasGroup>>?> fetchTamaGroups() async {
     final uuid = await _supabase.rpc('fetch_data_tracking', params: {'filter_tracking_name': 'tamas_group'});
     if (uuid == _persistantDataService.tamasGroupValue) return null;
     _persistantDataService.tamasGroupValue = uuid;
     logI('fetching new pins');
-    final data = await _supabase.rpc('fetch_all_tamas_groups');
+    final data = await _supabase.rpc('fetch_all_tamas_groups_new');
     if (data == null) return null;
 
-    final tamasGroups = <TamasGroup>[];
+    final tamasGroups = <String, List<TamasGroup>>{};
+    tamasGroups[kPremiumTamasGroups] = <PremiumTamasGroup>[];
+    tamasGroups[kRegularTamasGroups] = <TamasGroup>[];
     for (final map in data) {
-      tamasGroups.add(TamasGroup.fromJson(json: map));
+      if (_isJsonPremiumTama(map)) {
+        tamasGroups[kPremiumTamasGroups]!.add(PremiumTamasGroup.fromJson(json: map));
+      } else {
+        tamasGroups[kRegularTamasGroups]!.add(TamasGroup.fromJson(json: map));
+      }
     }
     return tamasGroups;
+  }
+
+  bool _isJsonPremiumTama(Map<String, dynamic> json) {
+    if (json.containsKey('background_color') &&
+        json.containsKey('price') &&
+        json.containsKey('video_url') &&
+        json['background_color'] != null &&
+        json['price'] != null &&
+        json['video_url'] != null) return true;
+
+    return false;
   }
 
   @override
